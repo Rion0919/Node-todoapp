05c3255c052b49f02442176f047a6e45a257eccc
Rion <rion.it456@gmail.com>
2022-05-27 20:30:15 +0900

    mod

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return mongoose
-    .connect(url)
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('データベースと接続中'))
     .catch((err) => console.log(err));
 };
